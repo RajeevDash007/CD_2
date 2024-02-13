bd4cddfccc1946124ce4cad5bd67be302a1a0bfd
Rajeev <rajeevdash07@gmail.com>
2024-02-13 18:00:00 +0530

    mapped the notes with the piano audio

--- static/tracker.js
+++ static/tracker.js
@@ -25,3 +25,21 @@ const sequence = [
   "Sa",
   "Sa",
 ].map((note) => noteMap[note]);
+let currentNoteIndex = 0;
+let isPlaying = false;
+
+const sampler = new Tone.Sampler({
+  urls: {
+    C4: "C4.mp3",
+    D4: "D4.mp3",
+    E4: "E4.mp3",
+    F4: "F4.mp3",
+    G4: "G4.mp3",
+    A4: "A4.mp3",
+    B4: "B4.mp3",
+    C5: "C5.mp3",
+  },
+  release: 1,
+  baseUrl: "https://fuhton.com/piano-mp3/piano-mp3/",
+}).toDestination();
+
