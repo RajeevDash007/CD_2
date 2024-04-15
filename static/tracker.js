996ccfa69bb3a3ac7aa69c296b4530392d598f60
Rajeev <rajeevdash07@gmail.com>
2024-04-15 13:55:31 +0530

    added function to extract sargam from the xml raw files

--- static/tracker.js
+++ static/tracker.js
@@ -8,399 +8,188 @@ WebMidi.enable(function (err) {
 });
 
 function init() {
+  // Initialize Tone.js
+  Tone.start();
+
   const noteMap = {
+    ml: "F3",
+    Ml: "Gb3",
+    pl: "G3",
+    Dl: "Ab3",
+    dl: "A3",
+    Nl: "Bb3",
+    nl: "B3",
     s: "C4",
+    R: "Db4",
     r: "D4",
-    g: "E4",
+    G: "D#4",
+    g: "Eb4",
     m: "F4",
+    M: "Gb4",
     p: "G4",
+    D: "Ab4",
     d: "A4",
+    N: "Bb4",
     n: "B4",
     su: "C5",
+    Ru: "Db5",
     ru: "D5",
-    gu: "E5",
+    Gu: "D#5",
+    gu: "Eb5",
+    mu: "F5",
+    Mu: "Gb5",
+    pu: "G5",
+    Du: "Ab5",
+    du: "A5",
+    Nu: "Bb5",
+    nu: "B5",
   };
-  
+
   const synth = new Tone.Sampler({
-    urls: {
-      C4: 'https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/C3.mp3',
-      D4: 'https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/D3.mp3',
-      E4: 'https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/E3.mp3',
-      F4: 'https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/F3.mp3',
-      G4: 'https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/G3.mp3',
-      A4: 'https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/A3.mp3',
-      B4: 'https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/B3.mp3',
-      C5: 'https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/C4.mp3',
-      D5: 'https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/D4.mp3',
-      E5: 'https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/E4.mp3',
-    },
-    release: 2,
-}).toDestination();
-  
-  const midiToNoteMap = {
-    60: "s", // C4
-    62: "r", // D4
-    64: "g", // E4
-    65: "m", // F4
-    67: "p", // G4
-    69: "d", // A4
-    71: "n", // B4
-    72: "su", // C5
-    74: "ru", // D5
-    76: "gu", // D5
-    // Add more mappings as needed
-  };
-  
-  const sequence = [
-    "su",
-    "su",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "r",
-    "g",
-    "r",
-    "p",
-    "g",
-    "d",
-    "p",
-    "g",
-    "g",
-    "su",
-    "su",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "r",
-    "g",
-    "r",
-    "p",
-    "g",
-    "d",
-    "p",
-    "g",
-    "g",
-    "g",
-    "p",
-    "d",
-    "su",
-    "ru",
-    "su",
-    "d",
-    "p",
-    "su",
-    "p",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "s",
-    "g",
-    "p",
-    "d",
-    "su",
-    "ru",
-    "su",
-    "d",
-    "p",
-    "su",
-    "p",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "s",
-    "g",
-    "g",
-    "p",
-    "d",
-    "p",
-    "su",
-    "su",
-    "su",
-    "d",
-    "d",
-    "su",
-    "ru",
-    "gu",
-    "ru",
-    "su",
-    "d",
-    "g",
-    "g",
-    "p",
-    "d",
-    "p",
-    "su",
-    "su",
-    "su",
-    "d",
-    "d",
-    "su",
-    "ru",
-    "gu",
-    "ru",
-    "su",
-    "d",
-    "gu",
-    "gu",
-    "ru",
-    "su",
-    "ru",
-    "ru",
-    "su",
-    "d",
-    "su",
-    "su",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "s",
-    "gu",
-    "gu",
-    "ru",
-    "su",
-    "ru",
-    "ru",
-    "su",
-    "d",
-    "su",
-    "su",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "s",
-  ].map((note) => noteMap[note]);
-  
+    "F3": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/F2.mp3",
+    "Gb3": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/Fs2.mp3",
+    "G3": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/G2.mp3",
+    "Ab3": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/Gs2.mp3",
+    "A3": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/A2.mp3",
+    "Bb3": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/As2.mp3",
+    "B3": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/B2.mp3",
+    "C4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/C3.mp3",
+    "Db4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/Cs3.mp3",
+    "D4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/D3.mp3",
+    "Eb4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/Ds3.mp3",
+    "E4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/E3.mp3",
+    "F4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/F3.mp3",
+    "Gb4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/Fs3.mp3",
+    "G4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/G3.mp3",
+    "Ab4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/Gs3.mp3",
+    "A4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/A3.mp3",
+    "Bb4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/As3.mp3",
+    "B4": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/B3.mp3",
+    "C5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/C4.mp3",
+    "Db5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/Cs4.mp3",
+    "D5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/D4.mp3",
+    "Eb5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/Ds4.mp3",
+    "E5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/E4.mp3",
+    "F5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/F4.mp3",
+    "Gb5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/Fs4.mp3",
+    "G5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/G4.mp3",
+    "Ab5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/Gs4.mp3",
+    "A5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/A4.mp3",
+    "Bb5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/As4.mp3",
+    "B5": "https://raw.githubusercontent.com/nbrosowsky/tonejs-instruments/master/samples/harmonium/B4.mp3",
+  }).toDestination();
+
+  let sequence = [];
+
   let currentNoteIndex = 0;
   let isPlaying = false;
-  
-  const inputs = WebMidi.inputs;
-  if (inputs.length > 0) {
-    const input = inputs[0];
-    input.addListener('noteon', 'all', onMIDIMessage);
-  } else {
-    console.log('No MIDI devices available.');
-  }
-  
-  
+
   const notesContainer = document.getElementById("notes-container");
-  const originalSequence = [
-    "su",
-    "su",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "r",
-    "g",
-    "r",
-    "p",
-    "g",
-    "d",
-    "p",
-    "g",
-    "-",
-    "su",
-    "su",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "r",
-    "g",
-    "r",
-    "p",
-    "g",
-    "d",
-    "p",
-    "g",
-    "-",
-    "g",
-    "p",
-    "d",
-    "su",
-    "ru",
-    "su",
-    "d",
-    "p",
-    "su",
-    "p",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "-",
-    "g",
-    "p",
-    "d",
-    "su",
-    "ru",
-    "su",
-    "d",
-    "p",
-    "su",
-    "p",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "-",
-    "g",
-    "g",
-    "p",
-    "d",
-    "p",
-    "su",
-    "-",
-    "su",
-    "d",
-    "d",
-    "su",
-    "ru",
-    "gu",
-    "ru",
-    "su",
-    "d",
-    "g",
-    "g",
-    "p",
-    "d",
-    "p",
-    "su",
-    "-",
-    "su",
-    "d",
-    "d",
-    "su",
-    "ru",
-    "gu",
-    "ru",
-    "su",
-    "d",
-    "gu",
-    "gu",
-    "ru",
-    "su",
-    "ru",
-    "ru",
-    "su",
-    "d",
-    "su",
-    "su",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "-",
-    "gu",
-    "gu",
-    "ru",
-    "su",
-    "ru",
-    "ru",
-    "su",
-    "d",
-    "su",
-    "su",
-    "d",
-    "p",
-    "g",
-    "r",
-    "s",
-    "-",
-  ];
-  originalSequence.forEach((note) => {
-    const noteElement = document.createElement("div");
-    noteElement.classList.add("note");
-    noteElement.textContent = note;
-    notesContainer.appendChild(noteElement);
-});
-  
-  document.getElementById("play").addEventListener("click", play);
+
+  document
+    .getElementById("play")
+    .addEventListener("click", () => play(sequence));
   document.getElementById("pause").addEventListener("click", pause);
   document.getElementById("stop").addEventListener("click", stop);
-  
-  function onMIDIMessage(event) {
-    const note = event.note.number;
-    const appNote = midiToNoteMap[note];
-    if (appNote) {
-      playAppNote(appNote);
-    }
+
+  // Function to handle XML file input
+  document
+    .getElementById("xmlInput")
+    .addEventListener("change", handleFileSelect);
+
+  function handleFileSelect(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+      const xmlText = event.target.result;
+      parseXML(xmlText);
+    };
+
+    reader.readAsText(file);
   }
-  
-  function playAppNote(appNote) {
-    const noteToPlay = noteMap[appNote];
-    if (noteToPlay && isPlaying) {
-      synth.triggerAttackRelease(noteToPlay, "0.5s");
-    }
-  }
-  
-  function playNote() {
-    if (currentNoteIndex >= sequence.length) {
-        stop();
-        return;
-    }
 
-    if (!isPlaying) {
-        return;
+  function parseXML(xmlText) {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlText, "text/xml");
+    const contentTags = xmlDoc.getElementsByTagName("CONTENT");
+    const newSequence = [];
+
+    for (let i = 0; i < contentTags.length; i++) {
+      const content = contentTags[i].childNodes[0].nodeValue;
+      const notes = content.trim().split(" ");
+      newSequence.push(...notes);
     }
 
-    const note = sequence[currentNoteIndex];
-    synth.triggerAttackRelease(note, "0.5s");
+    // Clear existing notes
+    notesContainer.innerHTML = "";
 
-    document.querySelectorAll(".note").forEach((el, i) => {
-        el.classList.toggle("active", i === currentNoteIndex);
+    // Add new notes to sequence and frontend
+    newSequence.forEach((note, index) => {
+      const noteElement = document.createElement("div");
+      noteElement.classList.add("note");
+      noteElement.textContent = note;
+      noteElement.setAttribute("data-seq-index", index); // Set a data attribute for the sequence index
+      notesContainer.appendChild(noteElement);
     });
-    currentNoteIndex++;
 
-    if (currentNoteIndex % 16 === 0) {
-        scrollPage(200); // Scroll the page by 50px after every 8 notes
-    }
+    // Update the sequence with new notes
+    sequence.length = 0;
+    sequence.push(...newSequence);
+  }
 
-    setTimeout(() => {
-        if (isPlaying) {
-            playNote();
-        }
-    }, 500);
-}
-
-function scrollPage(scrollAmount) {
-    window.scrollBy(0, scrollAmount);
-}
-  
-  function play() {
-    if (!isPlaying) {
+  function play(sequence) {
+    if (!isPlaying && sequence.length > 0) {
       isPlaying = true;
-      Tone.start();
       playNote();
     }
   }
-  
+
+  function playNote() {
+    if (currentNoteIndex >= sequence.length) {
+      stop();
+      return;
+    }
+
+    const appNote = sequence[currentNoteIndex];
+    const noteToPlay = noteMap[appNote];
+    if (noteToPlay) {
+      // Trigger the animation right before playing the sound
+      animateNote(appNote); // Move this line before synth.triggerAttackRelease
+      synth.triggerAttackRelease(noteToPlay, "0.5s");
+    }
+
+    currentNoteIndex++;
+
+    setTimeout(() => {
+      if (isPlaying) {
+        playNote();
+      }
+    }, 500); // This timing controls the gap between notes, adjust as necessary
+  }
+
+  function animateNote() {
+    const noteElement = document.querySelector(`.note[data-seq-index="${currentNoteIndex}"]`);
+    if (noteElement && !noteElement.classList.contains("active")) {
+        noteElement.classList.add("active");
+        setTimeout(() => {
+            noteElement.classList.remove("active");
+        }, 500); // Match this duration with the note play duration for synchronization
+
+        // Scroll the note into view
+        noteElement.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+}
+
   function pause() {
     isPlaying = false;
   }
-  
+
   function stop() {
     isPlaying = false;
     currentNoteIndex = 0;
-    document
-      .querySelectorAll(".note")
-      .forEach((noteElement) => noteElement.classList.remove("active"));
+    document.querySelectorAll(".note").forEach((noteElement) => {
+      noteElement.classList.remove("active");
+    });
   }
 }
